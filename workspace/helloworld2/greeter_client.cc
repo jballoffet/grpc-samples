1455418a8a76f6a3a24db180878937204a8088c5
Javier Balloffet <javier.balloffet@gmail.com>
2020-07-29 23:36:32 -0300

    Hello World projects cleanup

--- workspace/helloworld2/greeter_client.cc
+++ workspace/helloworld2/greeter_client.cc
@@ -36,24 +36,13 @@ class GreeterClient {
   GreeterClient(std::shared_ptr<Channel> channel)
       : stub_(Greeter::NewStub(channel)) {}
 
-  // Assembles the client's payload, sends it and presents the response back
-  // from the server.
   std::string SayHello(const std::string& user) {
-    // Data we are sending to the server.
     HelloRequest request;
     request.set_name(user);
-
-    // Container for the data we expect from the server.
     HelloReply reply;
-
-    // Context for the client. It could be used to convey extra information to
-    // the server and/or tweak certain RPC behaviors.
     ClientContext context;
 
-    // The actual RPC.
     Status status = stub_->SayHello(&context, request, &reply);
-
-    // Act upon its status.
     if (status.ok()) {
       return reply.message();
     } else {
@@ -86,10 +75,6 @@ class GreeterClient {
 };
 
 int main(int argc, char** argv) {
-  // Instantiate the client. It requires a channel, out of which the actual RPCs
-  // are created. This channel models a connection to localhost:50051.
-  // We indicate that the channel isn't authenticated (use of
-  // InsecureChannelCredentials()).
   std::string target_str("localhost:50051");
   GreeterClient greeter(grpc::CreateChannel(
       target_str, grpc::InsecureChannelCredentials()));
--- workspace/helloworld2/greeter_server.cc
+++ workspace/helloworld2/greeter_server.cc
@@ -34,7 +34,6 @@ using helloworld::HelloRequest;
 using helloworld::HelloReply;
 using helloworld::Greeter;
 
-// Logic and data behind the server's behavior.
 class GreeterServiceImpl final : public Greeter::Service {
   Status SayHello(ServerContext* context, const HelloRequest* request,
                   HelloReply* reply) override {
@@ -57,18 +56,13 @@ void RunServer() {
 
   grpc::EnableDefaultHealthCheckService(true);
   grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+
   ServerBuilder builder;
-  // Listen on the given address without any authentication mechanism.
   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  // Register "service" as the instance through which we'll communicate with
-  // clients. In this case it corresponds to an *synchronous* service.
   builder.RegisterService(&service);
-  // Finally assemble the server.
   std::unique_ptr<Server> server(builder.BuildAndStart());
   std::cout << "Server listening on " << server_address << std::endl;
 
-  // Wait for the server to shutdown. Note that some other thread must be
-  // responsible for shutting down the server for this call to ever return.
   server->Wait();
 }
 
